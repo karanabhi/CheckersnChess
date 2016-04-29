21d3e907f1c661ed431e741d323f56a8d18944a9
Abhishek Karan <karan_abhi@live.in>
2016-04-29 14:05:44 +0530

    Finished Assignment

--- Code.py
+++ Code.py
@@ -13,3 +13,11 @@ def minimax(self, currentState, alpha, beta): utility = currentState.utility() i
 bestAction = None for (action, succ) in currentState.successors(): 
 val = self.maximin(succ, alpha, beta)[1] if val ¡ beta: 
 bestAction, beta = action, val if alpha ¿= beta: break return bestAction, beta 
+
+
+
+Pos FirstUnassignedCell(MyCell[,] branch) for (int i = 0; i ¡ mgridSize; i+1) for (int j = 0; j ¡ mgridSize; j+1) if (!branch[i, j].assigned) return new Pos(i, j); return null; 
+Pos MinimumRemainingValues(MyCell[,] branch) int min = mgridSize + 1; 
+Pos p=new Pos() ; 
+for (int i = 0; i ¡ mgridSize; i++) for (int j = 0; j ¡ mgridSize; j++) if ((!branch[i, j].assigned) and (branch[i, j].Value.Length ¡ min)) p.ln = i; 
+p.col = j; min = branch[i, j].Value.Length; return p; char LeastConstraintValue(MyCell[,] branch, Pos variablePosition) int[] arr = new int[branch[variablePosition.ln, variablePosition.col].Value.Length]; for (int i = 0; i ¡ arr.Length; i++) arr[i] = 0; for (int i = 0; i ¡ branch[variablePosition.ln, variablePosition.col].Value.Length; i++) for (int j = 0; j ¡ branch[variablePosition.ln, variablePosition.col].Peers.Count; j++) if (branch[branch[variablePosition.ln, variablePosition.col].Peers[j].ln, branch[variablePosition.ln, variablePosition.col].Peers[j].col].Value.Contains( branch[variablePosition.ln, variablePosition.col].Value[i].ToString())) arr[i]++; return branch[variablePosition.ln, variablePosition.col].Value[GetMinIndex(arr)]; 
